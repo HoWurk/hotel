e5b2fb02b23a928ab47461aa7c281dcf5c7aa1cc
Oleg Zagryvyi <olegzagraf@gmail.com>
2023-09-04 18:44:52 +0300

    fixup! change api

--- src/main/java/com/hotel/hotelbooking/controller/BookingController.java
+++ src/main/java/com/hotel/hotelbooking/controller/BookingController.java
@@ -7,11 +7,12 @@ import com.hotel.hotelbooking.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
